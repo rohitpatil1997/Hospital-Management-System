a2a3392c6ef05ad90fc8d48a50510fd14d3aa9c8
rohitpatil1997 <rp888867@gmail.com>
2020-06-17 22:01:53 +0530

    Added Client Side Validation

--- Angular/src/PatientApp/PatientApp.Model.ts
+++ Angular/src/PatientApp/PatientApp.Model.ts
@@ -1,5 +1,31 @@
+import {NgForm,
+    FormGroup,
+        FormControl,
+            Validators,
+FormBuilder
+} from '@angular/forms';
+
+import { from } from 'rxjs';
+
 export class PatientModel{
     id:number = 0;
     name:string = "";
     problemDescription:string = "";
+
+    formPatientGroup:FormGroup = null;
+    constructor(){
+        
+        //tree structure
+        var _builder = new FormBuilder();
+        // use the builder and create the tree structure
+        this.formPatientGroup = _builder.group({});  
+        // add validation to formgroup
+        this.formPatientGroup.addControl("namecontrol", new FormControl('', Validators.required));
+        // add problem description
+        this.formPatientGroup.addControl("problemdescriptioncontrol", new FormControl('', Validators.required));
+
+
+     }
+    
+
 }
--- Angular/src/PatientApp/PatientApp.module.ts
+++ Angular/src/PatientApp/PatientApp.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { HttpClientModule } from '@angular/common/http';
 
@@ -15,6 +15,7 @@ import { from } from 'rxjs';
     RouterModule,
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [],
